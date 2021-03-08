4a2803d85367771d660b11e0e1ac63e350b0170d
Rowena-h <rowena.c.hill@gmail.com>
2021-03-08 16:56:43 +0000

    add blobtools.sh

---
+++ assembly/assessment/blobtools.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#$ -cwd           	# Set the working directory for the job to the current directory
+#$ -pe smp 1 		# Request 4 cors
+#$ -l h_rt=1:00:00 	# Request 30 min runtime
+#$ -l h_vmem=3G   	# Request 1GB RAM
+#$ -j y
+#$ -m bea
+#$ -t 1-5
+
+STRAIN=$(sed -n ${SGE_TASK_ID}p ../strains)
+
+module load samtools
+
+samtools index ../polishing/fusotu${STRAIN}_abyss_mapped_sorted_dups.bam
+
+module load anaconda3
+conda activate blobtools
+
+~/Programmes/blobtools/blobtools create 	-i ../denovo_assembly/abyss/fusotu${STRAIN}/fusotu${STRAIN}-contigs.fa \
+						-b ../polishing/fusotu${STRAIN}_abyss_mapped_sorted_dups.bam \
+						-t fusotu${STRAIN}_abyss_blast.tsv \
+						-o fusotu${STRAIN}_abyss
+
+~/Programmes/blobtools/blobtools plot 	-r species \
+					-i fusotu${STRAIN}_abyss.blobDB.json
