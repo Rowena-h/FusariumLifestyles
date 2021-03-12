9893d875b93a1bd3c16ae33f27c3730327a8e1e7
Rowena-h <rowena.c.hill@gmail.com>
2021-03-12 15:29:17 +0000

    Add annotation scripts

---
+++ annotation/repeat_masking/repeatmodeler.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#$ -cwd           	# Set the working directory for the job to the current directory
+#$ -pe smp 5		# Request 4 cores
+#$ -l h_rt=24:00:0 	# Request 24 hours runtime
+#$ -l h_vmem=1G   	# Request 1GB RAM
+#$ -j y
+#$ -m bea
+#$ -t 1-5
+
+STRAIN=$(sed -n ${SGE_TASK_ID}p ../strains)
+
+mkdir fusotu${STRAIN}
+
+cd fusotu${STRAIN}
+
+module load singularity
+
+singularity exec /data/containers/repeatmodeler/repeatmodeler-2.0.1.simg BuildDatabase -name fusotu${STRAIN}_abyss ../../../assembly/polishing/fusotu${STRAIN}_abyss_pilon.fasta
+
+singularity exec /data/containers/repeatmodeler/repeatmodeler-2.0.1.simg RepeatModeler -database fusotu${STRAIN}_abyss -engine ncbi -pa 1 -LTRStruct >& fusotu${STRAIN}_abyss.out 
