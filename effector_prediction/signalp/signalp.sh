4cb1bde44501f0d466d3a69c0405669e56f918d0
Rowena-h <rowena.c.hill@gmail.com>
2021-03-15 17:01:10 +0000

    Update

--- effector_prediction/signalp/signalp.sh
+++ effector_prediction/signalp/signalp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #$ -cwd 		# Set the working directory for the job to the current directory
 #$ -pe smp 1    	# Request 48 cores
-#$ -l h_rt=24:00:0     	# Request 72 hours runtime
+#$ -l h_rt=48:00:0     	# Request 72 hours runtime
 #$ -l h_vmem=5G         # Request 1GB RAM
 #$ -j y
 #$ -m bea
@@ -16,5 +16,5 @@ do
 	./signalp -fasta ${DIR}${i} -org euk -prefix ${i}_signalp
 	#List of signal peptide genes
         cat ${i}_signalp_summary.signalp5 | awk '$2=="SP(Sec/SPI)" {print $1}' > ${i}_signalp_SPlist
-	#mv ${i}_signalp* /data/SBCS-BuggsLab/RowenaHill/fus_comparison/signalp/
+	mv ${i}_signalp* /data/SBCS-BuggsLab/RowenaHill/fus_comparison/effector_prediction/signalp/
 done
