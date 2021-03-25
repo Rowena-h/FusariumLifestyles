8b8ee066a8e860da79ab50a48a91b6fc01390334
Rowena-h <rowena.c.hill@gmail.com>
2021-03-25 12:10:25 +0000

    Update

--- selection/reroot.r
+++ selection/reroot.r
@@ -2,7 +2,7 @@
 args=commandArgs(trailingOnly=TRUE)
 
 #Test if there are two arguments: if not, return an error
-if (length(args)<2) {
+if (length(args)<3) {
   stop("Three arguments must be supplied: a tree first, an outgroup string second and the directory to save the rooted tree third.", call.=FALSE)
 } 
 
@@ -11,7 +11,9 @@ library(ape)
 tree <- read.tree(args[1])
 outgroup <- as.character(args[2])
 
+file <- sub(".*\\/", "", args[1])
+
 tree <- root(tree, outgroup, resolve.root=TRUE, edgelabel=TRUE)
-write.tree(tree, file=paste0(args[3], args[1], "_rooted"))
+write.tree(tree, file=paste0(args[3], file, "_rooted"))
 
 
