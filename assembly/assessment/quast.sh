61c338f39a8145833f1274c8f401f34d1735d431
Rowena-h <rowena.c.hill@gmail.com>
2021-09-13 11:29:16 +0100

    add assembly assessment scripts

---
+++ assembly/assessment/quast.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#$ -cwd           	# Set the working directory for the job to the current directory
+#$ -pe smp 4		# Request 4 cors
+#$ -l h_rt=0:30:0 	# Request 30 min runtime
+#$ -l h_vmem=1G   	# Request 1GB RAM
+#$ -m bea
+#$ -j y
+
+STRAINS=$(cat ../strains)
+
+module load anaconda3
+conda activate quast
+
+for STRAIN in $STRAINS
+do
+	quast.py ../polishing/fusotu${STRAIN}_abyss_pilon.fasta ../polishing/fusotu${STRAIN}_megahit_pilon.fasta ../polishing/fusotu${STRAIN}_spades_pilon.fasta -o fusotu${STRAIN}_abyss_quast_results -t ${NSLOTS} --fungus -l "ABySS v2.0.2, MEGAHIT v1.2.9, SPAdes v3.11.1"
+done
