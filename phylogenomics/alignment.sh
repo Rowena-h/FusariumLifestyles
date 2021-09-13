7a16dadaf40c871b9aee6958561eee47a1e6bca8
Rowena Hill <34376457+Rowena-h@users.noreply.github.com>
2021-09-13 17:16:37 +0100

    add separate alignment script

---
+++ phylogenomics/alignment.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#$ -cwd           	# Set the working directory for the job to the current directory
+#$ -pe smp 1		# Request 1 core
+#$ -l h_rt=00:30:00 # Request 30 minutes runtime
+#$ -l h_vmem=1G   	# Request 1GB RAM
+#$ -j y
+
+ORTHO=$(cat aln_list | sed 's/\.fa//' | sed -n ${SGE_TASK_ID}p)
+ORTHOFINDER_DIR=../orthology_inference/OrthoFinder/Results_Mar01/Single_Copy_Orthologue_Sequences
+
+#Align single copy orthogroups
+
+module load mafft
+
+mafft ${ORTHOFINDER_DIR}/${ORTHO}.fa > gene_trees/${ORTHO}_aln.fa
+
+#Fix names
+
+sed 's/.faa.*$//' gene_trees/${ORTHO}_aln.fa > gene_trees/${ORTHO}_aln_edit.fa
+
+#Trim alignments
+
+java -jar /data/home/btx494/Programmes/BMGE-1.12/BMGE.jar -i gene_trees/${ORTHO}_aln.fa -t AA -of gene_trees/${ORTHO}_alntrimmed.fa
+java -jar /data/home/btx494/Programmes/BMGE-1.12/BMGE.jar -i gene_trees/${ORTHO}_aln_edit.fa -t AA -o gene_trees/${ORTHO}_aln_edit_trimmed.phy
