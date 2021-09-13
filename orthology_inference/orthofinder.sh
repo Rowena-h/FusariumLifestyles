40d4e155a20ad6126ca7a244fbe8943297b93028
Rowena Hill <34376457+Rowena-h@users.noreply.github.com>
2021-09-13 14:32:59 +0100

    update comments

--- orthology_inference/orthofinder.sh
+++ orthology_inference/orthofinder.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #$ -cwd                 # Set the working directory for the job to the current directory
-#$ -pe smp 10    	# Request 48 cores
-#$ -l h_rt=48:00:0     # Request 72 hours runtime
+#$ -pe smp 10    	    # Request 10 cores
+#$ -l h_rt=48:00:0      # Request 48 hours runtime
 #$ -l h_vmem=1G         # Request 1GB RAM
 #$ -j y
 #$ -m bea
@@ -11,4 +11,4 @@ conda activate orthofinder
 
 ulimit -n 3944
 
-orthofinder -b OrthoFinder/Results_Feb19_1/ -f blah -t 9
+orthofinder -f . -t 9
