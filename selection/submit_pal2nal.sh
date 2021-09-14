9f047a86731b322c0377ef083cf62832051e74df
Rowena Hill <34376457+Rowena-h@users.noreply.github.com>
2021-09-14 13:36:12 +0100

    update file locations

--- selection/submit_pal2nal.sh
+++ selection/submit_pal2nal.sh
@@ -5,18 +5,17 @@ NUM_ORTHO=$(ls -1 ../phylogenomics/gene_trees/*_aln.fa | wc -l)
 
 cat gbff_files/GCA* > gbff_files/concat.gbff
 
-#rm errors.txt
-rm own_fus_transcripts.fa
+rm gbff_files/own_fus_transcripts.fa
 rm pal2nal_check
 
 for i in 1 3 5 6 7
 do
-	cat ../annotation/maker/round3/fusotu${i}_abyss_rnd3.maker.output/fusotu${i}_abyss_rnd3.all.maker.transcripts.fasta | sed "s/>/>fusotu${i}_abyss_rnd3.all.maker.proteins.faa_/g" >> own_fus_transcripts.fa
+	cat ../annotation/maker/round3/fusotu${i}_abyss_rnd3.maker.output/fusotu${i}_abyss_rnd3.all.maker.transcripts.fasta | sed "s/>/>fusotu${i}_abyss_rnd3.all.maker.proteins.faa_/g" >> gbff_files/own_fus_transcripts.fa
 done
 
-sed -i 's/ .*//' own_fus_transcripts.fa
+sed -i 's/ .*//' gbff_files/own_fus_transcripts.fa
 
-cat own_fus_transcripts.fa Ilysp1_GeneCatalog_transcripts_20121116.nt.fasta | sed 's/.*|/\>Ilysp1_GeneCatalog_proteins_20121116.faa_/' > tmp && mv tmp own_fus_transcripts.fa
+cat gbff_files/own_fus_transcripts.fa gbff_files/Ilysp1_GeneCatalog_transcripts_20121116.nt.fasta | sed 's/.*|/\>Ilysp1_GeneCatalog_proteins_20121116.faa_/' > tmp && mv tmp gbff_files/own_fus_transcripts.fa
 
 mkdir alignments alignments/codon trees
 
