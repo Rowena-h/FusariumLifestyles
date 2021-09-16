13cacf0ac71eac976d1c514d650e1c908e61c306
Rowena-h <rowena.c.hill@gmail.com>
2021-09-16 11:27:47 +0100

    fix prefix

--- assembly/reads/trimmomatic.sh
+++ assembly/reads/trimmomatic.sh
@@ -9,4 +9,4 @@ STRAIN=$(sed -n ${SGE_TASK_ID}p ../strains)
 
 module load trimmomatic
 
-java -jar /share/apps/centos7/trimmomatic/0.36/trimmomatic-0.36.jar PE -threads ${NSLOTS} -trimlog fusotu${STRAIN}-trimmomatic.log FUS_OTU${STRAIN}_1.fastq.gz FUS_OTU${STRAIN}_2.fastq.gz FUS_OTU${STRAIN}_1_trimmedpaired.fastq.gz FUS_OTU${STRAIN}_1_trimmedunpaired.fastq.gz FUS_OTU${STRAIN}_2_trimmedpaired.fastq.gz ${STRAIN}_2_trimmedunpaired.fastq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+java -jar /share/apps/centos7/trimmomatic/0.36/trimmomatic-0.36.jar PE -threads ${NSLOTS} -trimlog fusotu${STRAIN}-trimmomatic.log FUS_OTU${STRAIN}_1.fastq.gz FUS_OTU${STRAIN}_2.fastq.gz FUS_OTU${STRAIN}_1_trimmedpaired.fastq.gz FUS_OTU${STRAIN}_1_trimmedunpaired.fastq.gz FUS_OTU${STRAIN}_2_trimmedpaired.fastq.gz FUS_OTU${STRAIN}_2_trimmedunpaired.fastq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
