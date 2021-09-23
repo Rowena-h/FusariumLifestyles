efec64c8ea6745a05306fd01aa8470d1ec8fb934
Rowena-h <rowena.c.hill@gmail.com>
2021-09-23 13:49:22 +0100

    update for NCBI compliance

---
+++ annotation/maker/round3/gag.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#$ -cwd                 # Set the working directory for the job to the current directory
+#$ -pe smp 1            # Request 1 core
+#$ -l h_rt=1:00:0       # Request 1 hour runtime
+#$ -l h_vmem=1G         # Request 1GB RAM
+#$ -j y
+#$ -t 1-5
+
+STRAIN=$(sed -n ${SGE_TASK_ID}p ../../strains)
+
+python2.7 ~/Programmes/genomeannotation-GAG-997e384/gag.py \
+-f ../../../assembly/polishing/fusotu${STRAIN}_abyss_pilon_filtered.fa \
+-g fusotu${STRAIN}_abyss_rnd3.maker.output/fusotu${STRAIN}_abyss_rnd3.all.maker.gff \
+-ris 10 \
+--fix_terminal_ns \
+--fix_start_stop \
+-o fusotu${STRAIN}_gag
+
+cd fusotu${STRAIN}_gag
+rename genome fusotu${STRAIN} *
+mv fusotu${STRAIN}.fasta fusotu${STRAIN}.fsa
+cd ../
