87d46745e5e7ab5d2b7da350d5c470d6f7a73aa5
Rowena-h <rowena.c.hill@gmail.com>
2021-10-14 12:50:51 +0100

    update directory

--- phylogenomics/submit_alignment.sh
+++ phylogenomics/submit_alignment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ls -1 ../orthology_inference/OrthoFinder/Results_Mar01/Single_Copy_Orthologue_Sequences/*.fa | sed s/^.*\\/\// > aln_list
+ls -1 ../orthology_inference/OrthoFinder/Results_Sep23/Single_Copy_Orthologue_Sequences/*.fa | sed s/^.*\\/\// > aln_list
 
 NUM=$(cat aln_list | wc -l)
 
