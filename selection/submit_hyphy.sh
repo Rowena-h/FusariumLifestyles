114720d9ab8f4410c6fbc40578fb247ccdaaad6d
Rowena-h <rowena.c.hill@gmail.com>
2021-10-29 13:46:43 +0100

    update for latest analysis

--- selection/submit_hyphy.sh
+++ selection/submit_hyphy.sh
@@ -5,23 +5,17 @@ ORTHO=$(cat ../phylogenomics/aln_list | sed 's/\.fa//')
 #Number of samples
 NUM_ORTHO=$(cat ../phylogenomics/aln_list | wc -l)
 
-mkdir hyphy hyphy/busted hyphy/meme hyphy/absrel hyphy/contrast-fel
+mkdir hyphy/busted hyphy/absrel hyphy/contrast-fel
 
-#Translate nucleotide alignments for plotting sites under selection
-#module load emboss
-
-#for i in $ORTHO
-#do
-#	transeq alignments/codon/${i}_aln_nuc.fa alignments/codon/${i}_aln_nuc.translated
-#	sed -i 's/_1$//' alignments/codon/${i}_aln_nuc.translated
-#
-#	#Label gene tree foreground branches for HyPhy
-#	sed -r 's/:/\{FOREGROUND\}:/g; s/\{FOREGROUND\}([^,]*)$/\1/' trees/${i}.raxml.bestTree_rooted > trees/${i}.raxml.bestTree_rooted_hyphy
-#done
+for i in $ORTHO
+do
+	#Label gene tree foreground branches for HyPhy
+	sed -r 's/:/\{FOREGROUND\}:/g; s/\{FOREGROUND\}([^,]*)$/\1/' trees/${i}.raxml.bestTree_rooted > trees/${i}.raxml.bestTree_rooted_hyphy
+done
 
 #Label dated species tree foreground branches for aBSREL and BUSTED
-sed -n 638p ../divergence_time_estimation/mcmctree/run1_independent/mcmctree_step2_out.txt | sed -r 's/:/\{FOREGROUND\}:/g; s/\{FOREGROUND\}([^,]*)$/\1/' > trees/dated_tree_absrel.tre
-awk -F',' '{print $8 "\t" $9}' ../metadata.csv > tmp
+sed -n 631p ../divergence_time_estimation/mcmctree/run1_independent/mcmctree_step2_out.txt | sed -r 's/:/\{FOREGROUND\}:/g; s/\{FOREGROUND\}([^,]*)$/\1/' > trees/dated_tree_absrel.tre
+awk -F',' '{print $10 "\t" $11}' ../metadata.csv > tmp
 sed `cat tmp | awk '{print "-e s/"$2"/"$1"/"}'`<<<"`cat trees/dated_tree_absrel.tre`" > tmp2 && mv tmp2 trees/dated_tree_absrel.tre
 rm tmp
 
@@ -30,7 +24,9 @@ module load R
 
 Rscript label_trees.r
 
+cd hyphy 
+
 #Submit HyPhy programmes
-qsub -t 1-${NUM_ORTHO} hyphy/BUSTED.sh
-qsub -t 1-${NUM_ORTHO} hyphy/aBSREL.sh
-qsub -t 1-${NUM_ORTHO} hyphy/Contrast-FEL.sh
+qsub -t 1-${NUM_ORTHO} BUSTED.sh
+qsub -t 1-${NUM_ORTHO} aBSREL.sh
+qsub -t 1-${NUM_ORTHO} Contrast-FEL.sh
